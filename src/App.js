56f5cb4fbaa5e13ae18137b7107078425ad33ea1
dhannjayyy <d.dhannjayyy@gmail.com>
2022-06-30 02:00:14 +0530

    scaffolding

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'rsuite/dist/styles/rsuite-default.css';
+import './styles/main.scss';
+
 
 function App() {
   return (
