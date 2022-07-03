a77fec486d5132ff95a137255b78b32b89986bee
dhannjayyy <d.dhannjayyy@gmail.com>
2022-07-04 00:00:00 +0530

    started building dashboard, added sign out button, displayed user name on drawer

--- src/components/Dashboard/DashboardToggle.jsx
+++ src/components/Dashboard/DashboardToggle.jsx
@@ -1,21 +1,29 @@
 /* eslint-disable arrow-body-style */
-import React from 'react'
-import { Button, Drawer, Icon } from 'rsuite'
+import React, { useCallback } from 'react'
+import { Alert, Button, Drawer, Icon } from 'rsuite'
 import Dashboard from '.'
 import { useMediaQuery, useModalState } from '../../misc/customHooks'
+import { auth } from '../../misc/firebase'
 
 const DashboardToggle = () => {
 
     const { isOpen, closeDrawer, openDrawer } = useModalState()
     const isMobile = useMediaQuery('(max-width:992px)')
-    
+
+    const onSignOut = useCallback(() => {
+        auth.signOut();
+        Alert.info("Signed Out",4000);
+
+        closeDrawer();
+}, [closeDrawer]);
+
     return (
         <>
             <Button block color="blue" onClick={openDrawer}>
                 <Icon icon="dashboard" />
             </Button>
             <Drawer full={isMobile} show={isOpen} onHide={closeDrawer} placement="left">
-                <Dashboard />
+                <Dashboard onSignOut={onSignOut}/>
             </Drawer>
         </>
     )
--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,9 +1,25 @@
 /* eslint-disable arrow-body-style */
 import React from 'react'
+import { Button, Drawer } from 'rsuite';
+import { useProfile } from '../../context/profile.context';
 
-const Dashboard = () => {
+
+const Dashboard = ({ onSignOut }) => {
+  const { profile } = useProfile();
   return (
-    <div>Dashboard</div>
+    <>
+      <Drawer.Header>
+        <Drawer.Title>
+          Dashboard
+        </Drawer.Title>
+      </Drawer.Header>
+      <Drawer.Body>
+        <h3>Hey, {profile.name}</h3>
+      </Drawer.Body>
+      <Drawer.Footer>
+        <Button block color="red" onClick={onSignOut}>Sign Out</Button>
+      </Drawer.Footer>
+    </>
   )
 }
 
