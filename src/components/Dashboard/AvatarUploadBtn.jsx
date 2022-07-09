b37f0e213f7b23295c94597aaca68ccd4c1fcdfd
dhannjayyy <d.dhannjayyy@gmail.com>
2022-07-10 00:40:20 +0530

    displayed user avatar on dashboard

--- src/components/Dashboard/AvatarUploadBtn.jsx
+++ src/components/Dashboard/AvatarUploadBtn.jsx
@@ -6,6 +6,7 @@ import AvatarEditor from 'react-avatar-editor'
 import { useModalState } from '../../misc/customHooks';
 import { database,storage } from '../../misc/firebase'
 import { useProfile } from '../../context/profile.context';
+import ProfileAvatar from '../ProfileAvatar';
 
 const fileInputTypes = ".png, .jpeg, .jpg";
 const acceptedFileTypes = ['image/png', 'image/jpeg', 'image/pjpeg'];
@@ -71,6 +72,7 @@ const AvatarUploadBtn = () => {
 
     return (
         <div className='mt-3 text-center'>
+        <ProfileAvatar src={profile.avatar} name={profile.name} className="width-200 height-200 img-fullsize font-huge"/>
             <div>
                 <label htmlFor='avatar-upload' className='d-block cursor-pointer padded'>
                     Select new avatar
---
+++ src/components/ProfileAvatar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Avatar } from 'rsuite'
+import { getNameInitials } from '../misc/helpers'
+
+
+
+const ProfileAvatar = ({ name, ...avatarProps }) => {
+  return ( 
+    <Avatar circle{...avatarProps}>
+    {getNameInitials(name)}
+    </Avatar>
+  )
+}
+
+export default ProfileAvatar
