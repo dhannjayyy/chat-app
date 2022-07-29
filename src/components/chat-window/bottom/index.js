ed6fae4426c8081b7a33698269c11bd63787e68f
dhannjayyy <d.dhannjayyy@gmail.com>
2022-07-29 14:12:33 +0530

    Created chat bottom, send messages in the chat

--- src/components/chat-window/bottom/index.js
+++ src/components/chat-window/bottom/index.js
@@ -1,8 +1,82 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+import { Icon, Input, InputGroup, Alert } from 'rsuite'
+import InputGroupButton from 'rsuite/lib/InputGroup/InputGroupButton'
+import firebase from 'firebase/app'
+import { useParams } from 'react-router'
+import { useProfile } from '../../../context/profile.context'
+import { database } from '../../../misc/firebase'
+
+
+function assembleMessage(profile, chatId) {
+  return {
+    roomId: chatId,
+    author: {
+      name: profile.name,
+      uid: profile.uid,
+      createdAt: profile.createdAt,
+      ...(profile.avatar ? { avatar: profile.avatar } : {})
+    },
+    createdAt: firebase.database.ServerValue.TIMESTAMP
+  }
+}
 
 const ChatBottom = () => {
+  const [input, setInput] = useState('');
+  const [isLoading, setIsLoading] = useState(false)
+
+  const { chatId } = useParams();
+  const { profile } = useProfile()
+
+  const onInputChange = useCallback((value) => {
+    setInput(value);
+  }, [])
+
+  
+  const onSendClick = async () => {
+    
+    if (input.trim() === '') {
+      return
+    }
+    
+    const msgData = assembleMessage(profile, chatId);
+    msgData.text = input;
+    
+    const updates = {};
+
+    const messageId = database.ref('messages').push().key;
+    updates[`/messages/${messageId}`] = msgData;
+    updates[`/rooms/${chatId}/lastMessage`] = {
+      ...msgData,
+      msdId: messageId,
+    };
+    
+    setIsLoading(true);
+    try {
+      await database.ref().update(updates);
+      setInput('');
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      Alert.error(error.message);
+    }
+
+  }
+  
+  const onKeyDown = (ev) =>{
+    if(ev.keyCode===13){
+      ev.preventDefault();
+      onSendClick();
+    }
+  }
   return (
-    <div>ChatBottom</div>
+    <div>
+      <InputGroup>
+        <Input placeholder="Write a message here..." onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
+        <InputGroup.Button color="blue" appearance='primary' onClick={onSendClick} disabled={isLoading}>
+          <Icon icon="send" />
+        </InputGroup.Button>
+      </InputGroup>
+    </div>
   )
 }
 
--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -11,7 +11,7 @@ const ChatTop = () => {
   return (
     <div>
       <div className='d-flex justify-content-between align-items-center'>
-        <h4>
+        <h4 className='text-disappear d-flex align-items-center'>
           <Icon componentClass={Link} to="/" icon="arrow-circle-left" size="2x" className={isMobile ? 'd-inline-block p-0 mr-2 text-blue link-unstyled' : 'd-none'} />
           <span className='text-disappear'>
             {name}
