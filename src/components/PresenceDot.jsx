d6b275b23ed2ca75f85ecccbd1cb8d85e89bca79
dhannjayyy <d.dhannjayyy@gmail.com>
2022-07-30 00:17:26 +0530

    Displayed real time presence

---
+++ src/components/PresenceDot.jsx
@@ -0,0 +1,45 @@
+import { getByText } from '@testing-library/react';
+import React from 'react'
+import { Tooltip, Whisper, Badge } from 'rsuite';
+import { usePresence } from '../misc/customHooks'
+
+
+const getColor = (presence) => {
+    if (!presence) {
+        return 'gray';
+    }
+    switch (presence.state) {
+        case 'online': return "green";
+        case 'offline': return "red";
+        default: return 'gray';
+    }
+}
+
+const getText = (presence) => {
+    if (!presence) {
+        return 'Unknown state';
+    }
+
+    return presence.state === 'online' ? 'Online' : `Last online ${new Date(presence.last_changed).toLocalDateString()}`
+}
+
+const PresenceDot = ({ uid }) => {
+
+    const presence = usePresence(uid);
+
+    return (
+
+        <Whisper placement='top' trigger="hover" speaker={
+            <Tooltip>
+                {getText(presence)}
+            </Tooltip>
+        }
+        >
+            <Badge className='cursor-pointer' style={{ backgroundColor: getColor(presence) }} />
+
+        </Whisper>
+
+    )
+}
+
+export default PresenceDot
--- src/components/chat-window/messages/MessageItem.jsx
+++ src/components/chat-window/messages/MessageItem.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import TimeAgo from 'timeago-react';
+import PresenceDot from '../../PresenceDot';
 import ProfileAvatar from '../../ProfileAvatar';
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 
 
-const MessageItem = ({message}) => {
-    const { author, createdAt, text } = message;
+const MessageItem = ({ message }) => {
+  const { author, createdAt, text } = message;
   return (
     <li className='padded mb-1'>
-    <div className='d-flex align-items-center font-bolder mb-1'>
-    <ProfileAvatar src={author.avatar} name={author.name} className='ml-1' size="xs" />
-    <ProfileInfoBtnModal profile={author} appearance="link" className='p-0 ml-1 text-black'/>&nbsp;&nbsp;
-    <TimeAgo datetime={createdAt} className='font-normal text-black-45' />
-    </div>
-        <div>
-            <span className='word-break-all'>{text}</span>
-        </div>
+      <div className='d-flex align-items-center font-bolder mb-1'>
+
+        <PresenceDot uid={author.uid} />
+
+        <ProfileAvatar src={author.avatar} name={author.name} className='ml-1' size="xs" />
+        <ProfileInfoBtnModal profile={author} appearance="link" className='p-0 ml-1 text-black' />&nbsp;&nbsp;
+        <TimeAgo datetime={createdAt} className='font-normal text-black-45' />
+      </div>
+      <div>
+        <span className='word-break-all'>{text}</span>
+      </div>
     </li>
   )
 }
