7395bb8a83f8d260a6c1298fd70bd8b8e1926c18
dhannjayyy <d.dhannjayyy@gmail.com>
2022-09-01 22:10:08 +0530

    Handle delete message

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -76,40 +76,40 @@ const Messages = () => {
     Alert.info(alertMsg,4000);
   },[])
 
-// const handleDelete = useCallback( async (msgId)=>{
-//   if( !window.confirm('Delete this message ? ')){
-//     return;
-//   }
-//   const isLast = messages[messages.length - 1].id === msgId;
-//   const updates = {};
+const handleDelete = useCallback(async (msgId)=>{
+  if( !window.confirm('Delete this message ? ')){
+    return;
+  }
+  const isLast = messages[messages.length - 1].id === msgId;
+  const updates = {};
 
-//   updates[`/messages/${msgId}`] = null;
+  updates[`/messages/${msgId}`] = null;
 
-//   if(isLast && messages.length>1){
-//     updates[`/rooms/${chatId}/lastMessage`] = {
-//       ...messages[messages.length - 2],
-//       msgId : messages[messages.length - 2].id
-//     }
-//   }
+  if(isLast && messages.length>1){
+    updates[`/rooms/${chatId}/lastMessage`] = {
+      ...messages[messages.length - 2],
+      msgId : messages[messages.length - 2].id
+    }
+  }
 
-//   if(isLast && messages.length === 1){
-//     updates[`/rooms/${chatId}/lastMessage`] = null;
-//   }
+  if(isLast && messages.length === 1){
+    updates[`/rooms/${chatId}/lastMessage`] = null; 
+  }
 
-//   try {
-//     await database.ref().update(updates);
-//     Alert.info('Message has been deleted')
-//   } catch (error) {
-//     Alert.error(error.message)
-//   }
+  try {
+    await database.ref().update(updates);
+    Alert.info('Message has been deleted')
+  } catch (error) {
+    Alert.error(error.message)
+  }
 
-// },[chatId,messages])
+},[chatId,messages])
 
   return (
     <ul className='msg-list custom-scroll '>
 
       {isChatEmpty && <li>No messages yet</li>}
-      {canShowMessages && messages.map(msg => (<MessageItem key={msg.id} message={msg} handleAdmin={handleAdmin} handleLike={handleLike} />))}
+      {canShowMessages && messages.map(msg => (<MessageItem key={msg.id} message={msg} handleAdmin={handleAdmin} handleLike={handleLike} handleDelete={handleDelete}/>))}
 
     </ul>
   )
